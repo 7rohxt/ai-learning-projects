eec805323e0c8a832cb37d6055a2e704f266b3e2
Rohit Karthick <rkbeo5@gmail.com>
2025-04-10 17:57:50 +0530

    performed z score transformation and added to scripts

--- preprocessing.py
+++ preprocessing.py
@@ -1,3 +1,6 @@
+import numpy as np
+import pandas as pd
+
 def preprocessing(data):
     # Drop rows with missing values
     data.dropna(inplace=True)
@@ -16,8 +19,14 @@ def preprocessing(data):
     data.drop('ocean_proximity', axis=1, inplace=True)
 
     # Create new features
-    data['rooms_per_household'] = data['log_total_bedrooms'] / data['log_total_rooms']
-    data['bedroom_ratio'] = data['log_total_bedrooms'] / data['log_total_bedrooms']
+    data['rooms_per_household'] = data['log_totalrooms'] / data['log_households']
+    data['bedroom_ratio'] = data['log_total_bedrooms'] / data['log_total_rooms']
     data['population_per_household'] = data['log_population'] / data['log_households']
+
+    # z-score transformation
+    for j in data.columns:
+        mean = data[j].mean()
+        std_dev = data[j].std()
+        data[j] = (data[j] - mean) / std_dev 
 
     return data
