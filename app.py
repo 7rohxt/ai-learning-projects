350426cbc23e53896a1a59e1bdb8f0d7ef93f8f8
Rohit Karthick <rkbeo5@gmail.com>
2025-05-10 13:06:09 +0530

    feat: added api to predict house price

--- app.py
+++ app.py
@@ -1,14 +1,39 @@
 from flask import Flask, request, jsonify
-from utils import load_data, load_model, get_unique_ocean_proximity
+from utils import load_data, load_model, get_unique_ocean_proximity, predict_home_price
 import pandas as pd
 
 app = Flask(__name__)
 data = load_data()
 model = load_model()
 
+
 @app.route('/get_ocean_proximity')
 def get_ocean_proximity():
     return jsonify(get_unique_ocean_proximity(data))
+
+@app.route('/predict_price', methods=['POST'])
+def predict_price():
+    user_input = request.get_json()
+
+    try:
+        prediction = predict_home_price(
+            housing_median_age=user_input["housing_median_age"],
+            total_rooms=user_input["total_rooms"],
+            total_bedrooms=user_input["total_bedrooms"],
+            population=user_input["population"],
+            households=user_input["households"],
+            median_income=user_input["median_income"],
+            ocean_proximity=user_input.get("ocean_proximity", "INLAND"),
+            longitude=user_input.get("longitude", 36.5),
+            latitude=user_input.get("latitude", -119.5),
+            model=model
+        )
+        return jsonify({"predicted_price": prediction})
+    except KeyError as e:
+        return jsonify({"error": f"Missing field: {e.args[0]}"}), 400
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 
 if __name__ == "__main__":
     print("Starting")
