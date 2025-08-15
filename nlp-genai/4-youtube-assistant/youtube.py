bc39adcb83b23bc5b8b8186d0e2d0eee0a8b5bb5
Rohit Karthick <rkbeo5@gmail.com>
2025-08-15 16:41:55 +0530

    build vector DB from YouTube transcription

--- nlp-genai/4-youtube-assistant/youtube.py
+++ nlp-genai/4-youtube-assistant/youtube.py
@@ -1,7 +1,12 @@
 from dotenv import load_dotenv
 from youtube_transcript_api import YouTubeTranscriptApi
+from langchain.schema import Document
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
 
 load_dotenv()
+embeddings = OpenAIEmbeddings()
 
 def get_transcript_text(video_url: str, languages=['en']) -> str:
     video_id = video_url.split("v=")[-1]
@@ -11,5 +16,18 @@ def get_transcript_text(video_url: str, languages=['en']) -> str:
     fetched = transcript.fetch()
     return " ".join([snippet.text for snippet in fetched])
 
+def create_db_from_youtube_video_url(video_url: str) -> FAISS:
+
+    transcript_text = get_transcript_text(video_url)
+
+    docs = [Document(page_content=transcript_text, metadata={"source": video_url})]
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+    split_docs = text_splitter.split_documents(docs)
+
+    db = FAISS.from_documents(split_docs, embeddings)
+    return db
+
 video_url = "https://www.youtube.com/watch?v=Pqb19IaWwOE"
-print(get_transcript_text(video_url))
+db = create_db_from_youtube_video_url(video_url)
+print(db)
